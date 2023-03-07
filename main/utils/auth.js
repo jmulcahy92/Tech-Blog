31451fc21c88bbc2ef6dc6cb42dba0c0c75acb75
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-03-06 19:04:25 -0600

    starter code

---
+++ main/utils/auth.js
@@ -0,0 +1,10 @@
+// PROVIDED AS STARTER CODE
+const withAuth = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
---
+++ main/utils/helpers.js
@@ -0,0 +1,6 @@
+// PROVIDED AS STARTER CODE
+module.exports = {
+  format_date: date => {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  }
+};
