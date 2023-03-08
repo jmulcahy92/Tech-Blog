920df3ff7113e40bbe00d96742dfaf13b5e94d0d
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-03-08 00:59:08 -0600

    first route functional

--- main/controllers/homeRoutes.js
+++ main/controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -13,10 +13,12 @@ router.get('/', async (req, res) => {
           },
         ],
       });
-  
+
       // Serialize data so the template can read it
       const posts = postData.map((post) => post.get({ plain: true }));
   
+      console.log(posts);
+
       // Pass serialized data and session flag into template
       res.render('homepage', { 
         posts, 
