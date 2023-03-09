a8d187b7d5fd0c5517111d6dec328bdd598a77bf
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-03-09 01:44:39 -0600

    get post by id; no comments

--- main/controllers/homeRoutes.js
+++ main/controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -31,6 +31,32 @@ router.get('/', async (req, res) => {
 
 router.get('/dashboard', (req, res) => {
   res.render('dashboard');
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['body']
+        }
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
