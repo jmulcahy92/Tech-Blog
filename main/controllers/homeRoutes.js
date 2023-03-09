9c6a4e21d8db5e11f6fa052a9bcd40e03c54b5ac
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-03-08 23:54:11 -0600

    signup, login, logout

--- main/controllers/homeRoutes.js
+++ main/controllers/homeRoutes.js
@@ -27,6 +27,20 @@ router.get('/', async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
-  });
+});
 
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+  
 module.exports = router;
